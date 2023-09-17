a5cb4d97a53eca007a65cc2e5cdfd4d8e13e52ae
Eli Hermann <Eli_Hermann@hotmail.com>
2023-09-16 23:19:15 -0700

    working

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import urllib
 
 from googleapiclient.discovery import build
 
+# Every day I want to just open my laptop and run this program and let it add all the comments that it can.
+
 
 def get_video_id(youtube_url):
     """Gets the video ID from the YouTube URL."""
@@ -16,19 +18,62 @@ def get_video_id(youtube_url):
     return video_id
 
 
-# Get the YouTube API service object
-youtube = build('youtube', 'v3', developerKey="")
+def query_comments(video_id, youtube, next_page_token):
+    # Get the comments for the video
+    request = youtube.commentThreads().list(
+        part="snippet",
+        maxResults=100,
+        textFormat="plainText",
+        videoId=video_id,
+        pageToken=next_page_token
+    )
+    return request.execute()
 
-# Get the comments for the video
-comments = youtube.comments().list(part='snippet', videoId="U8U_gR58eJU").execute()
-# comments = youtube.comments().list(part='snippet', videoId="U8U_gR58eJU").execute()
 
-# Write the comments to a CSV file
-# with open('comments.csv', 'w', encoding='utf-8') as csvfile:
-#     fieldnames = ['comment_id', 'author_name', 'text', 'likes', 'kind', 'id', 'snippet', 'etag']
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-#     writer.writeheader()
-#     for comment in comments['items']:
-#         writer.writerow(comment)
-for comment in comments['items']:
-    print(comment['snippet']['topLevelComment']['snippet']['textDisplay'])
+def main():
+    # get developer key and video URL from config file
+    with open("config.json") as json_data_file:
+        config_data = json.load(json_data_file)
+
+    # Get the YouTube API service object
+    youtube = build('youtube', 'v3', developerKey=config_data['developer-key'])
+
+    # query for comments
+    video_id = get_video_id(config_data['video-url'])
+
+    while True:
+        response = query_comments(video_id, youtube, config_data['next-page-token'])
+
+        # Write nextPageToken to the config file
+        config_data['next-page-token'] = response['nextPageToken']
+        with open("config.json", "w") as outfile:
+            json.dump(config_data, outfile)
+
+        # Filter data
+        filtered_data = [
+            {
+                'id': item['id'],
+                'text': item['snippet']['topLevelComment']['snippet']['textOriginal'],
+                'publishedAt': item['snippet']['topLevelComment']['snippet']['publishedAt'],
+                'authorDisplayName': item['snippet']['topLevelComment']['snippet']['authorDisplayName']
+            }
+            for item in response['items']
+        ]
+
+        # write comments to csv
+        csv_filename = "comments.csv"
+
+        # Open the CSV file in append mode ('a') to add data to the end
+        with open(csv_filename, mode="a", newline="") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=["id", "text", "publishedAt", "authorDisplayName"])
+
+            # If the file is empty, write the header row
+            if csv_file.tell() == 0:
+                writer.writeheader()
+
+            # Write the data to the CSV file
+            writer.writerows(filtered_data)
+
+
+if __name__ == "__main__":
+    main()
